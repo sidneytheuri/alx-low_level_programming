1824df5a185c1094ca07fab342b3c41b5a394479
sidneytheuri <sidneytheuri.st@gmail.com>
2023-02-22 00:44:01 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-* main - check the code
-* print_alphabet - Prints the alphabet in lowercase
+* print_alphabet - Prints the alphabet in lowercase.
 * Return: Always 0 (Success)
 */
 void print_alphabet(void)

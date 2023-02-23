a60881dde5406fe4ac5536e3aed2d70fdafcdd3c
sidneytheuri <sidneytheuri.st@gmail.com>
2023-02-23 21:19:28 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,13 @@ char n, c;
 int i = 0;
 while (i < 10)
 {
-for (n = 0; n = 14; n++)
+for (n = 0; n <= 14; n++)
 {
 c = n;
 if (n > 9)
 {
-_putchar('1'); c = n % 10;
+_putchar('1');
+c = n % 10;
 }
 _putchar('0' + c);
 }

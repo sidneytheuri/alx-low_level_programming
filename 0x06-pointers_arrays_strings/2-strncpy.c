e7cc3827e1ddb8cf76c4fbf34793a16c198fc748
sidneytheuri <sidneytheuri.st@gmail.com>
2023-03-02 21:17:20 +0000

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_strncpy - makes copy of string
+*@dest: A character pointer
+*@src: Apointer to character 
+*@n: number
+*Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}

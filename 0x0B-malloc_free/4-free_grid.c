ccf98e61b3c289fd08afd0c1e2be668450971808
sidneytheuri <sidneytheuri.st@gmail.com>
2023-03-14 21:46:38 +0000

    free_grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,12 +8,14 @@
  */
 void free_grid(int **grid, int height)
 {
-int i;
+	int i;
 
-if (grid == NULL || height == 0)
-return;
-for (i = 0; i < height; i++)
-free(grid[i]);
-free(grid);
+	if (grid == NULL || height == 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
 }
 

59e58bd40d7a9c7838fd5ae0f239f383f5747cb5
sidneytheuri <sidneytheuri.st@gmail.com>
2023-04-10 11:10:24 -0700

    a fucntion  that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * create_file -  generates file
+ *@text_content: the info in file
+ *@filename: name of file
+ *Retrun: 1 if success and -1 on fail
+ */
+
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int t;
+	int tletters;
+	int perm;
+
+
+	if (!filename)
+		return (-1);
+
+	t = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+
+	if (t == -1)
+		return (-1);
+
+
+
+	if (!text_content)
+		text_content = "";
+
+
+	for (tletters = 0; text_content[tletters]; tletters++)
+		;
+
+	perm = write(t, text_content, tletters);
+
+
+	if (perm == -1)
+		return (-1);
+
+	close(t);
+return (1);
+}
+

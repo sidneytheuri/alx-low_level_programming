1d4e88c6cfd5585a5b13f3010ce546796c10ed5c
sidneytheuri <sidneytheuri.st@gmail.com>
2023-04-11 11:56:51 -0700

    correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 
 
-	if (p <= -1)
+	if (p == -1)
 		return (-1);
 
 	if (text_content)
@@ -35,7 +35,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 		ist = write(p, text_content, pletters);
 
-		if (ist <= -1)
+		if (ist == -1)
 			return (-1);
 	}
 	close(p);

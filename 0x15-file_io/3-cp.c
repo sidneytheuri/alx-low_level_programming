fb68f90417dfe5f3eb5e5da5dfb8e9855bc723cb
sidneytheuri <sidneytheuri.st@gmail.com>
2023-04-11 12:18:11 -0700

    correct

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,13 +2,13 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* file_error - check is file is accessible
-* @file_to: file
-* @file_from: source file
+* error_file - check is file is accessible
+* @file_from: file_from
+* @file_to: source file_to
 * @argv: argument
 * Return: NULL return
 */
-void file_error(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
 if (file_from == -1)
 	{
@@ -20,7 +20,7 @@ dprintf(STDERR_FILENO, "Error: No file to read from %s\n", argv[1]);
 	if (file_to == -1)
 
 	{
-		dprintf(STDERR_FILENO, "Error: no permission to write %s\n", argv[3]);
+		dprintf(STDERR_FILENO, "Error: no permission to write %s\n", argv[2]);
 				exit(99); }
 }
 
@@ -33,7 +33,7 @@ dprintf(STDERR_FILENO, "Error: No file to read from %s\n", argv[1]);
  */
 int main(int argc, char *argv[])
 {
-int file_to, file_from, close_error;
+int file_from, file_to, error_close;
 ssize_t tchar, ist;
 char buffer[1024];
 
@@ -44,25 +44,25 @@ exit(97);
 }
 file_from = open(argv[1], O_RDONLY);
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-file_error(file_from, file_to, argv);
+error_file(file_from, file_to, argv);
 tchar = 1024;
 while (tchar == 1024)
 {
 	tchar = read(file_from, buffer, 1024);
 	if (tchar == -1)
-		file_error(0, -1, argv);
+		error_file(0, -1, argv);
 	ist = write(file_to, buffer, tchar);
 	if (ist == -1)
-		file_error(0, -1, argv);
+		error_file(0, -1, argv);
 }
-close_error = close(file_from);
-if (close_error == -1)
+error_close = close(file_from);
+if (error_close == -1)
 {
 dprintf(STDERR_FILENO, "Error: cannot close t %d\n", file_from);
 	exit(100);
 }
-close_error = close(file_to);
-if (close_error == -1)
+error_close = close(file_to);
+if (error_close == -1)
 {
 dprintf(STDERR_FILENO, "Error: not able to close t %d\n", file_from);
 	exit(100);

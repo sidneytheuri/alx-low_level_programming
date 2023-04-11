58093959a65fa299e06a1c5b79ce15e690ad452d
sidneytheuri <sidneytheuri.st@gmail.com>
2023-04-11 11:44:58 -0700

    correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
  * read_textfile - reads a text file and prints the characters
  * @letters: total value of letters printed
@@ -10,36 +10,34 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
-int t;
-ssize_t ist, nst;
-char *buffer;
-
-
+int fd;
+ssize_t nrd, nwr;
+char *buf;
 
 if (!filename)
-	return (0);
-t = open(filename, O_RDONLY);
+return (0);
+
+
+fd = open(filename, O_RDONLY);
+
+if (fd == -1)
+return (0);
 
 
 
-if  (t == -1)
-	return (0);
+buf = malloc(sizeof(char) * (letters));
+if (!buf)
+return (0);
 
 
-buffer = malloc(sizeof(char) * (letters));
-if (!buffer)
-	return (0);
+nrd = read(fd, buf, letters);
+nwr = write(STDOUT_FILENO, buf, nrd);
 
-ist = read(t, buffer, letters);
-nst = write(STDOUT_FILENO, buffer, ist);
+close(fd);
 
 
-close(ist);
 
-free(buffer);
-
-
-return (nst);
+free(buf);
+return (nwr);
 }
 
